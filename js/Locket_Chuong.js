849fb844611403e7b0beb07d565b3bbe9a6050eb
ChuongLD <Lechuong652688@gmail.com>
2026-07-10 21:31:24 +0700

    First commit

---
+++ js/Locket_Chuong.js
@@ -0,0 +1,56 @@
+// ========= ID ========= //
+const mapping = {
+  "%E8%BD%A6%E7%A5%A8%E7%A5%A8": ["vip+watch_vip"],
+  Locket: ["Gold"],
+};
+
+// =========   Phần cố định  ========= //
+// =========  @Chuong ========= //
+
+var ua =
+    $request.headers["User-Agent"] ||
+    $request.headers["user-agent"],
+  obj = JSON.parse($response.body);
+
+obj.Attention =
+  "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+var chuong = {
+    is_sandbox: !1,
+    ownership_type: "PURCHASED",
+    billing_issues_detected_at: null,
+    period_type: "normal",
+    expires_date: "2099-12-18T01:04:17Z",
+    grace_period_expires_date: null,
+    unsubscribe_detected_at: null,
+    original_purchase_date: "2024-07-28T01:04:18Z",
+    purchase_date: "2024-07-28T01:04:17Z",
+    store: "app_store",
+  },
+  chuong = {
+    grace_period_expires_date: null,
+    purchase_date: "2024-07-28T01:04:17Z",
+    product_identifier: "com.chuong.premium.yearly",
+    expires_date: "2099-12-18T01:04:17Z",
+  };
+
+const match = Object.keys(mapping).find((e) => ua.includes(e));
+
+if (match) {
+  let [e, s] = mapping[match];
+
+  s
+    ? ((chuong.product_identifier = s),
+      (obj.subscriber.subscriptions[s] = chuong))
+    : (obj.subscriber.subscriptions["com.chuong.premium.yearly"] =
+        chuong);
+
+  obj.subscriber.entitlements[e] = chuong;
+} else {
+  obj.subscriber.subscriptions["com.chuong.premium.yearly"] = chuong;
+  obj.subscriber.entitlements.pro = chuong;
+}
+
+$done({
+  body: JSON.stringify(obj),
+});
